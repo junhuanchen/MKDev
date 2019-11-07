7a795b51641f9ee16bb71a0a7d7a589a63b92bed
Juwan <junhuanchen@qq.com>
2019-11-07 14:10:07 +0800

    update led blink

--- main.c
+++ main.c
@@ -9,11 +9,10 @@
 
 #include <stdio.h>
 #include <string.h>
-						   
-#include "ControlEvent.h"
-#include "crypt.h"							
-#include ".\Usb\Usb.h"
 
+#include "ControlEvent.h"
+#include "crypt.h"
+#include ".\Usb\Usb.h"
 
 void CheckUsbState()
 {
@@ -27,27 +26,41 @@ void CheckUsbState()
 
 static QueueArray xdata Array;
 
+#include ".\Public\GPIO.h"
+
 void main(void)
-{
+{				   
+  UINT16 Count = 0;
   static UINT8 Recv[MAX_PACKET_SIZE];
-  
-  CfgFsys();    // Configure sys
-  mDelaymS(5);  //
+
+  CfgFsys();   // Configure sys	  
+  mDelaymS(5); // Wait 	
+
+  Port3Cfg(1, GPIO_Mode_PP_Out); // UART0 TXD INIT
 
   // mInitSTDIO(), LOG("main ready\r\n"); //  Init UART0
-  
-  if (CheckMKDevice() == FALSE) while(1);
+
+  if (CheckMKDevice() == FALSE)
+    while (1)
+      ;
 
   ControlEventInit(&Array);
 
   USBDeviceInit();
-  
+
   CH554WDTModeSelect(1); // Start WDT
 
   HAL_ENABLE_INTERRUPTS();
-  
+
+
   while (1)
   {
+    if (Count++ > 0x4000)
+    {
+      TXD = !TXD;
+	  Count = 0;
+    }
+
     CH554WDTFeed(0);
 
     CheckUsbState();
